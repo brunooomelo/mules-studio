a5518606b3240babeaefdfacfa436b6777497d19
brunooomelo <bruno94@outlook.com>
2022-02-13 22:57:22 -0300

    UI improviment

--- pages/index.tsx
+++ pages/index.tsx
@@ -26,8 +26,17 @@ const Home: NextPage = () => {
       )}
       <main className="flex flex-col items-center bg-secondary py-20 xl:flex-row xl:rounded-2xl">
         <div className="">
+          <div className="px-4 text-center">
+            <h1 className="text-4xl sm:text-5xl font-bold text-white">
+              Welcome to Mule Studio
+            </h1>
+            <p className="w-3/4 mx-auto text-white my-5 text-center text-base sm:text-xl">
+              Mules is a collection of 1337 uniquely and randomly NFTs stored on
+              Fantom Network
+            </p>
+          </div>
           {!isEnableSale ? (
-            <div className="rounded-3xl border-4 border-white p-8 text-white font-bold md:text-5xl w-full text-center my-4 ">
+            <div className="rounded-3xl border-4 border-white p-8 text-white font-bold md:text-5xl w-3/4 mx-auto text-center my-4">
               <h1 className="">OPEN SALES IN</h1>
               <span>
                 {remainingTime.days} D : {remainingTime.hours} H :{" "}
@@ -35,52 +44,40 @@ const Home: NextPage = () => {
               </span>
             </div>
           ) : (
-            <>
-              <div className="px-4 text-center">
-                <h1 className="text-4xl sm:text-5xl font-bold text-white">
-                  Welcome to Mule Studio
-                </h1>
-                <p className="w-3/4 mx-auto text-white my-5 text-center text-base sm:text-xl">
-                  Mules is a collection of 1337 uniquely and randomly NFTs
-                  stored on Fantom Network
-                </p>
+            <div className="lg:flex w-full justify-center">
+              <div
+                className={`flex flex-col items-center ${
+                  checkChain && "mt-20"
+                } justify-around h-64 lg:w-96 xl:mt-10`}
+              >
+                {checkChain ? (
+                  <>
+                    <StatusMint supply={supply} />
+                    <Input
+                      decrease={decrease}
+                      onChange={setField}
+                      increment={increment}
+                      value={value}
+                    />
+                    <MintButton
+                      handleMint={onMint}
+                      loading={mint.loading}
+                      hasBalance={Boolean(balance > 1.5)}
+                      disabled={!isEnableSale}
+                      soldOut={isSoldOut}
+                    />
+                    <PriceMint />
+                  </>
+                ) : (
+                  <button
+                    className="max-w-[270px] px-14 py-2 rounded-xl bg-orange-400 text-white font-bold"
+                    onClick={changeNetwork}
+                  >
+                    Change to Fantom Opera Network
+                  </button>
+                )}
               </div>
-
-              <div className="lg:flex w-full justify-center">
-                <div
-                  className={`flex flex-col items-center ${
-                    checkChain && "mt-20"
-                  } justify-around h-64 lg:w-96 xl:mt-10`}
-                >
-                  {checkChain ? (
-                    <>
-                      <StatusMint supply={supply} />
-                      <Input
-                        decrease={decrease}
-                        onChange={setField}
-                        increment={increment}
-                        value={value}
-                      />
-                      <MintButton
-                        handleMint={onMint}
-                        loading={mint.loading}
-                        hasBalance={Boolean(balance > 1.5)}
-                        disabled={!isEnableSale}
-                        soldOut={isSoldOut}
-                      />
-                      <PriceMint />
-                    </>
-                  ) : (
-                    <button
-                      className="max-w-[270px] px-14 py-2 rounded-xl bg-orange-400 text-white font-bold"
-                      onClick={changeNetwork}
-                    >
-                      Change to Fantom Opera Network
-                    </button>
-                  )}
-                </div>
-              </div>
-            </>
+            </div>
           )}
         </div>
         <img className="lg:w-[450px]" src="preview.png" alt="preview mule" />
