3f5a69728a88cbbe1f76e14b9c854bc49ae777d9
brunooomelo <bruno94@outlook.com>
2022-02-15 10:18:02 -0300

    change text

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -1,13 +1,10 @@
 import { ConnectButton } from "../ConnectButton";
 
-export function Header () {
+export function Header() {
   return (
-    <div className='flex flex-col sm:flex-row sm:justify-between sm:px-10 items-center justify-around  h-28'>
-      <h1 className="text-3xl font-bold text-white">
-        Mule Studio
-      </h1> 
+    <div className="flex flex-col sm:flex-row sm:justify-between sm:px-10 items-center justify-around  h-28">
+      <h1 className="text-3xl font-bold text-white">Mules Studio</h1>
       <ConnectButton />
     </div>
-
-)
+  );
 }
